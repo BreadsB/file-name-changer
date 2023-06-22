27d8f3dcc1b38ea6d5ad947d075c038c157a5a9b
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-06-22 18:42:28 +0200

    all

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.breadsb.namechaner {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.breadsb.namechaner to javafx.fxml;
-    exports com.breadsb.namechaner;
-}
